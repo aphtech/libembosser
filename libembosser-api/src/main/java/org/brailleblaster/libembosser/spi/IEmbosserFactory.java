e0b40697c6da0faa0b1ba0b0759ea40d7e6aceb1
Michael Whapples <mwhapples@aim.com>
2018-01-02 06:46:53 +0000

    Better javadoc

--- libembosser-api/src/main/java/org/brailleblaster/libembosser/spi/IEmbosserFactory.java
+++ libembosser-api/src/main/java/org/brailleblaster/libembosser/spi/IEmbosserFactory.java
@@ -23,6 +23,8 @@ public interface IEmbosserFactory {
 	 * 
 	 * This method will get all the embossers supplied by this factory, but the instances will be localised according to the locale specified. In cases where an implementation does not have a localised form, either because it does not provide a localisation for the specified locale or because it does not make sense to localise the implementation (eg. if the only strings are product names and manufacturer names) then the factory should have a default fallback and supply that instead. This method should never throw an error due to the locale specified.
 	 * 
+	 * Implementations should also not mutate previously returned instances. To mutate previously returned instances may lead to unexpected results for clients where they find that the locale of existing embossers change without notice.
+	 * 
 	 * @param locale The locale to be used by driver instances returned.
 	 * @return A list of embossers.
 	 */
