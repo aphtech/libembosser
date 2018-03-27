5313ac9fbfe479e799d1dbfc825bcf55e4df12de
Michael Whapples <mwhapples@aim.com>
2018-03-27 06:48:16 +0100

    Add Z-folding sides modes for Juliet and Romeo embossers

--- libembosser-core-drivers/src/main/java/org/brailleblaster/libembosser/drivers/indexBraille/IndexBrailleFactory.java
+++ libembosser-core-drivers/src/main/java/org/brailleblaster/libembosser/drivers/indexBraille/IndexBrailleFactory.java
@@ -15,7 +15,7 @@ import com.google.common.collect.ImmutableList;
 public class IndexBrailleFactory implements IEmbosserFactory {
 	private static final EnumSet<MultiSides> BASIC_D_SIDES = EnumSet.of(MultiSides.INTERPOINT, MultiSides.P1ONLY, MultiSides.Z_FOLDING_DOUBLE_HORIZONTAL, MultiSides.Z_FOLDING_SINGLE_HORIZONTAL, MultiSides.Z_FOLDING_DOUBLE_VERTICAL, MultiSides.Z_FOLDING_SINGLE_VERTICAL);
 	private static final EnumSet<MultiSides> EVEREST_SIDES = EnumSet.of(MultiSides.INTERPOINT, MultiSides.P1ONLY, MultiSides.SADDLE_STITCH_DOUBLE_SIDED, MultiSides.SADDLE_STITCH_SINGLE_SIDED);
-	private static final EnumSet<MultiSides> ENABLING_SIDES = EnumSet.of(MultiSides.INTERPOINT, MultiSides.P1ONLY);
+	private static final EnumSet<MultiSides> ENABLING_SIDES = EnumSet.of(MultiSides.INTERPOINT, MultiSides.P1ONLY, MultiSides.Z_FOLDING_DOUBLE_VERTICAL, MultiSides.Z_FOLDING_SINGLE_VERTICAL, MultiSides.Z_FOLDING_DOUBLE_HORIZONTAL, MultiSides.Z_FOLDING_SINGLE_HORIZONTAL);
 	private static final Rectangle FOUR_BY_THREE_PAPER = new Rectangle(new BigDecimal("101.6"), new BigDecimal("76.2"));
 	private static final Rectangle TWELVE_AND_HALF_BY_TWENTY_FOUR_PAPER = new Rectangle(new BigDecimal("317.5"), new BigDecimal("609.6"));
 	private List<IEmbosser> embossers;
