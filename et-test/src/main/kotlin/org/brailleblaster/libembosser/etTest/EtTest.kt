ca06a7dd016b28328e4af5479583905d79118599
Michael Whapples <mwhapples@aim.com>
2018-10-15 17:20:36 +0100

    Change to use build.gradle rather than kotlin script.

--- et-test/src/main/kotlin/org/brailleblaster/libembosser/etTest/EtTest.kt
+++ et-test/src/main/kotlin/org/brailleblaster/libembosser/etTest/EtTest.kt
@@ -1,5 +1,9 @@
 package org.brailleblaster.libembosser.etTest
+ 
+import kotlin.io.readLine
 
 fun main(args: Array<String>) {
 	println("Test app for Enabling Technologies embossers")
+	val choice = readLine()
+	println(choice)
 }
