4e17fec6422712dbf2e95210f1491ec71003c9a4
Michael Whapples <mwhapples@aim.com>
2018-10-16 11:06:41 +0100

    Initial working implementation of ET Test application.

--- et-test/src/main/kotlin/org/brailleblaster/libembosser/etTest/EtTest.kt
+++ et-test/src/main/kotlin/org/brailleblaster/libembosser/etTest/EtTest.kt
@@ -1,9 +1,43 @@
 package org.brailleblaster.libembosser.etTest
  
 import kotlin.io.readLine
+import javax.print.PrintService
+import java.io.InputStream
 
 fun main(args: Array<String>) {
-	println("Test app for Enabling Technologies embossers")
-	val choice = readLine()
-	println(choice)
+	val testDocs = listOf(
+			",a test docu;t4".byteInputStream(),
+			",ano!r test docu;t4".byteInputStream()
+	)
+	val app = AppModel()
+	val ui = CliApp()
+	var exitApp = false
+	val testOptions = testDocs.mapIndexed { index, doc ->
+		val testNum = (index + 1).toString()
+		UserOption(testNum, "Test ${testNum}", {
+			a -> a.inStream = doc
+			a.print()
+		})
+	} + UserOption("q", "Quit", { _ -> ui.quit() })
+	ui.selectPrinter()(app)
+	do {
+		ui.selectTest(testOptions)(app)
+	} while (!exitApp)
+}
+
+class CliApp {
+	fun quit(exitCode: Int = 0) {
+		System.exit(exitCode)
+	}
+	fun selectPrinter(): AppAction {
+			val printerList = getPrinters().mapIndexed { index, printer ->
+			UserOption((index + 1).toString(), printer.name, { app -> app.printer = printer })
+		}
+		println("Select a embosser\n")
+		return userSelection(printerList).action
+	}
+	fun selectTest(docOptions: List<UserOption>): AppAction {
+		println("Select a test document\n")
+		return userSelection(docOptions).action
+	}
 }
---
+++ et-test/src/main/kotlin/org/brailleblaster/libembosser/etTest/Utils.kt
@@ -0,0 +1,46 @@
+package org.brailleblaster.libembosser.etTest
+
+import java.util.function.Consumer
+import javax.print.DocFlavor
+import javax.print.PrintService
+import javax.print.PrintServiceLookup
+import javax.print.SimpleDoc
+import java.io.InputStream
+
+class AppModel {
+	var printer: PrintService = getDefaultPrinter()
+	var inStream: InputStream = "".byteInputStream()
+	fun print(): Unit {
+		val doc = SimpleDoc(inStream, DocFlavor.INPUT_STREAM.AUTOSENSE, null)
+		val job = printer.createPrintJob()
+		job.print(doc, null)
+	}
+}
+
+typealias AppAction = (AppModel) -> Unit
+
+data class UserOption(val key: String, val value: String, val action: AppAction) {
+	constructor(key: String, value: String, action: Consumer<AppModel>) : this(key, value, { app -> action.accept(app) })
+}
+
+fun getDefaultPrinter(): PrintService {
+	return PrintServiceLookup.lookupDefaultPrintService()
+}
+
+fun getPrinters(): List<PrintService> {
+	return PrintServiceLookup.lookupPrintServices(DocFlavor.INPUT_STREAM.AUTOSENSE, null).asList()
+}
+
+fun userSelection(options:  List<UserOption>): UserOption {
+	for (option in options) {
+		println("${option.key} - ${option.value}")
+	}
+	print("\nPlease make a selection: ")
+	val selection = readLine()
+	val selectedOption = options.singleOrNull { it.key == selection }
+	println("")
+	return if (selectedOption != null) selectedOption else {
+		println("Invalid choice, please pick one of the options")
+		userSelection(options)
+	}
+}
