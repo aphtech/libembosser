2362dc2c31d7d894a5199baba0bba584e9ffb51c
Michael Whapples <mwhapples@aim.com>
2018-10-24 18:49:36 +0100

    Simple writing of lines from BRF.

--- libembosser-core-drivers/src/main/java/org/brailleblaster/libembosser/drivers/utils/DocumentParser.java
+++ libembosser-core-drivers/src/main/java/org/brailleblaster/libembosser/drivers/utils/DocumentParser.java
@@ -1,22 +1,79 @@
 package org.brailleblaster.libembosser.drivers.utils;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.io.InputStream;
+import java.util.List;
 
+import org.brailleblaster.libembosser.drivers.utils.DocumentHandler.BrailleEvent;
+import org.brailleblaster.libembosser.drivers.utils.DocumentHandler.DocumentEvent;
 import org.brailleblaster.libembosser.drivers.utils.DocumentHandler.EndDocumentEvent;
+import org.brailleblaster.libembosser.drivers.utils.DocumentHandler.EndLineEvent;
 import org.brailleblaster.libembosser.drivers.utils.DocumentHandler.EndPageEvent;
 import org.brailleblaster.libembosser.drivers.utils.DocumentHandler.EndSectionEvent;
 import org.brailleblaster.libembosser.drivers.utils.DocumentHandler.EndVolumeEvent;
 import org.brailleblaster.libembosser.drivers.utils.DocumentHandler.StartDocumentEvent;
+import org.brailleblaster.libembosser.drivers.utils.DocumentHandler.StartLineEvent;
 import org.brailleblaster.libembosser.drivers.utils.DocumentHandler.StartPageEvent;
 import org.brailleblaster.libembosser.drivers.utils.DocumentHandler.StartSectionEvent;
 import org.brailleblaster.libembosser.drivers.utils.DocumentHandler.StartVolumeEvent;
 
+import com.google.common.base.Charsets;
+import com.google.common.collect.ImmutableList;
+
 public class DocumentParser {
-	public void parseBrf(InputStream input, DocumentHandler handler) {
+	private static class LineBuilder extends ByteArrayOutputStream {
+		public LineBuilder() {
+			this(100);
+		}
+		public LineBuilder(int size) {
+			super(size);
+		}
+		public List<DocumentEvent> getLineEvents() {
+			if (count == 0) {
+				return ImmutableList.of();
+			} else {
+				return ImmutableList.of(new StartLineEvent(), new BrailleEvent(new String(buf, 0, count, Charsets.US_ASCII)), new EndLineEvent());
+			}
+		}
+	}
+	/**
+	 * Parse the BRF passing the document events to handler.
+	 * 
+	 * @param input The InputStream of the BRF.
+	 * @param handler The handler to recieve document events.
+	 * @throws IOException Thrown when there is a problem reading the input stream. Should this happen then the handler probably will not be in the READY state and so cannot be reused without being reset.
+	 */
+	public void parseBrf(InputStream input, DocumentHandler handler) throws IOException {
 		handler.onEvent(new StartDocumentEvent());;
 		handler.onEvent(new StartVolumeEvent());;
 		handler.onEvent(new StartSectionEvent());;
 		handler.onEvent(new StartPageEvent());;
+		final int bufferSize = 4096;
+		byte[] buffer = new byte[bufferSize];
+		LineBuilder line = new LineBuilder();
+		int bytesRead;
+		while ((bytesRead = input.read(buffer)) > 0) {
+			for (int i = 0; i < bytesRead; ++i) {
+				byte b = buffer[i];
+				switch(b) {
+				case '\f':
+				case '\n':
+				case '\r':
+					for (DocumentEvent event: line.getLineEvents()) {
+						handler.onEvent(event);
+					}
+					line.reset();
+					break;
+				default:
+					line.write(new byte[] {b});
+				}
+			}
+		}
+		for (DocumentEvent event: line.getLineEvents()) {
+			handler.onEvent(event);
+		}
+		line.close();
 		handler.onEvent(new EndPageEvent());;
 		handler.onEvent(new EndSectionEvent());;
 		handler.onEvent(new EndVolumeEvent());;
--- libembosser-core-drivers/src/test/java/org/brailleblaster/libembosser/drivers/utils/DocumentParserTest.java
+++ libembosser-core-drivers/src/test/java/org/brailleblaster/libembosser/drivers/utils/DocumentParserTest.java
@@ -2,8 +2,10 @@ package org.brailleblaster.libembosser.drivers.utils;
 
 import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertTrue;
+import static org.testng.Assert.fail;
 
 import java.io.ByteArrayInputStream;
+import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.Iterator;
@@ -11,14 +13,17 @@ import java.util.List;
 import java.util.Optional;
 import java.util.Set;
 
+import org.brailleblaster.libembosser.drivers.utils.DocumentHandler.BrailleEvent;
 import org.brailleblaster.libembosser.drivers.utils.DocumentHandler.DocumentEvent;
 import org.brailleblaster.libembosser.drivers.utils.DocumentHandler.EndDocumentEvent;
+import org.brailleblaster.libembosser.drivers.utils.DocumentHandler.EndLineEvent;
 import org.brailleblaster.libembosser.drivers.utils.DocumentHandler.EndPageEvent;
 import org.brailleblaster.libembosser.drivers.utils.DocumentHandler.EndSectionEvent;
 import org.brailleblaster.libembosser.drivers.utils.DocumentHandler.EndVolumeEvent;
 import org.brailleblaster.libembosser.drivers.utils.DocumentHandler.Option;
 import org.brailleblaster.libembosser.drivers.utils.DocumentHandler.OptionEvent;
 import org.brailleblaster.libembosser.drivers.utils.DocumentHandler.StartDocumentEvent;
+import org.brailleblaster.libembosser.drivers.utils.DocumentHandler.StartLineEvent;
 import org.brailleblaster.libembosser.drivers.utils.DocumentHandler.StartPageEvent;
 import org.brailleblaster.libembosser.drivers.utils.DocumentHandler.StartSectionEvent;
 import org.brailleblaster.libembosser.drivers.utils.DocumentHandler.StartVolumeEvent;
@@ -26,6 +31,8 @@ import org.brailleblaster.libembosser.drivers.utils.DocumentHandler.ValueOption;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
+import com.google.common.base.Charsets;
+import com.google.common.base.Strings;
 import com.google.common.collect.ImmutableList;
 
 public class DocumentParserTest {
@@ -33,14 +40,26 @@ public class DocumentParserTest {
 	public Iterator<Object[]> brfProvider() {
 		List<Object[]> data = new ArrayList<>();
 		// Test an empty document
-		data.add(new Object[] {new ByteArrayInputStream(new byte[0]), ImmutableList.of(new StartDocumentEvent(), new StartVolumeEvent(), new StartSectionEvent(), new StartPageEvent(), new EndPageEvent(), new EndSectionEvent(), new EndVolumeEvent(), new EndDocumentEvent())});
+		final ImmutableList<DocumentEvent> minimumDocumentEvents = ImmutableList.of(new StartDocumentEvent(), new StartVolumeEvent(), new StartSectionEvent(), new StartPageEvent(), new EndPageEvent(), new EndSectionEvent(), new EndVolumeEvent(), new EndDocumentEvent());
+		data.add(new Object[] {new ByteArrayInputStream(new byte[0]), minimumDocumentEvents});
+		// A blank page potentially may be just the formfeed 
+		data.add(new Object[] {new ByteArrayInputStream(new byte[] {(byte)'\f'}), minimumDocumentEvents});
+		// Sometimes a blank page may just contain blank lines
+		data.add(new Object[] {new ByteArrayInputStream(Strings.repeat("\r\n", 24).concat("\f").getBytes(Charsets.US_ASCII)), minimumDocumentEvents});
+		// Braille character bytes should be copied to output
+		final ImmutableList<DocumentEvent> basicDocumentEvents = ImmutableList.of(new StartDocumentEvent(), new StartVolumeEvent(), new StartSectionEvent(), new StartPageEvent(), new StartLineEvent(), new BrailleEvent("TEST Braille"), new EndLineEvent(), new EndPageEvent(), new EndSectionEvent(), new EndVolumeEvent(), new EndDocumentEvent());
+		data.add(new Object[] {new ByteArrayInputStream("TEST Braille".getBytes(Charsets.US_ASCII)), basicDocumentEvents});
 		return data.iterator();
 	}
 	@Test(dataProvider="brfProvider")
 	public void testParseBrf(InputStream input, List<DocumentEvent> expectedEvents) {
 		DocumentParser parser = new DocumentParser();
 		final List<DocumentEvent> actualEvents = new ArrayList<>();
-		parser.parseBrf(input, e -> actualEvents.add(e));
+		try {
+			parser.parseBrf(input, e -> actualEvents.add(e));
+		} catch (IOException e) {
+			fail("Problem parsing the BRF", e);
+		}
 		assertEquals(actualEvents.size(), expectedEvents.size(), "Not got the expected number of events");
 		for (int i = 0; i < expectedEvents.size(); ++i) {
 			DocumentEvent expectedEvent = expectedEvents.get(i);
@@ -60,6 +79,8 @@ public class DocumentParserTest {
 						assertEquals(((ValueOption<?>)actualOption.get()).getValue(), ((ValueOption<?>)expectedOption).getValue());
 					}
 				}
+			} else if (expectedEvent instanceof BrailleEvent) {
+				assertEquals(((BrailleEvent)actualEvent).getBraille(), ((BrailleEvent)expectedEvent).getBraille(), "Braille is not expected Braille.");
 			}
 		}
 	}
