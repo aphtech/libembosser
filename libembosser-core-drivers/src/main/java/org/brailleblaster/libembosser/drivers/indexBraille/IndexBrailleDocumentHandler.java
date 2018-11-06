004a2bc01f1756a31c28c545cfa7cee9757109f3
Michael Whapples <mwhapples@aim.com>
2018-11-06 15:45:47 +0000

    Add support for paper modes to IndexBrailleDocumentHandler

--- libembosser-core-drivers/src/main/java/org/brailleblaster/libembosser/drivers/indexBraille/IndexBrailleDocumentHandler.java
+++ libembosser-core-drivers/src/main/java/org/brailleblaster/libembosser/drivers/indexBraille/IndexBrailleDocumentHandler.java
@@ -2,6 +2,7 @@ package org.brailleblaster.libembosser.drivers.indexBraille;
 
 import org.brailleblaster.libembosser.drivers.generic.GenericTextDocumentHandler;
 import org.brailleblaster.libembosser.drivers.utils.DocumentHandler;
+import org.brailleblaster.libembosser.spi.MultiSides;
 
 import com.google.common.base.Charsets;
 import com.google.common.io.ByteSource;
@@ -13,6 +14,7 @@ public class IndexBrailleDocumentHandler implements DocumentHandler {
 		private int cellsPerLine = 40;
 		private int linesPerPage = 25;
 		private int copies = 1;
+		private int paperMode = 1;
 		public Builder setLeftMargin(int leftMargin) {
 			this.leftMargin = leftMargin;
 			return this;
@@ -33,13 +35,45 @@ public class IndexBrailleDocumentHandler implements DocumentHandler {
 			this.copies = copies;
 			return this;
 		}
+		public Builder setPaperMode(MultiSides sides) {
+			switch(sides) {
+			case P1ONLY:
+			case P2ONLY:
+				paperMode = 1;
+				break;
+			case INTERPOINT:
+				paperMode = 2;
+				break;
+			case Z_FOLDING_DOUBLE_HORIZONTAL:
+				paperMode = 3;
+				break;
+			case SADDLE_STITCH_DOUBLE_SIDED:
+				paperMode = 4;
+				break;
+			case Z_FOLDING_SINGLE_HORIZONTAL:
+				paperMode = 5;
+				break;
+			case Z_FOLDING_DOUBLE_VERTICAL:
+				paperMode = 6;
+				break;
+			case Z_FOLDING_SINGLE_VERTICAL:
+				paperMode = 7;
+				break;
+			case SADDLE_STITCH_SINGLE_SIDED:
+				paperMode = 8;
+				break;
+			default:
+				throw new IllegalArgumentException(String.format("Index embossers do not support %s paper mode", sides.name()));
+			}
+			return this;
+		}
 		public IndexBrailleDocumentHandler build() {
-			return new IndexBrailleDocumentHandler(leftMargin, topMargin, cellsPerLine, linesPerPage, copies);
+			return new IndexBrailleDocumentHandler(leftMargin, topMargin, cellsPerLine, linesPerPage, paperMode, copies);
 		}
 	}
 	private final GenericTextDocumentHandler textHandler;
 	private final ByteSource header;
-	private IndexBrailleDocumentHandler(int leftMargin, int topMargin, int cellsPerLine, int linesPerPage, int copies) {
+	private IndexBrailleDocumentHandler(int leftMargin, int topMargin, int cellsPerLine, int linesPerPage, int paperMode, int copies) {
 		this.textHandler = new GenericTextDocumentHandler.Builder()
 				.setLeftMargin(leftMargin)
 				.setTopMargin(topMargin)
@@ -47,7 +81,7 @@ public class IndexBrailleDocumentHandler implements DocumentHandler {
 				.setLinesPerPage(linesPerPage)
 				.setCopies(1) // Our header will provide the copies escape sequence, so no data duplication needed.
 				.build();
-		String headerString = String.format("\u001bDBT0,MC%d,BI%d,CH%d,TM%d,LP%d;", copies, leftMargin, cellsPerLine, topMargin, linesPerPage);
+		String headerString = String.format("\u001bDBT0,MC%d,DP%d,BI%d,CH%d,TM%d,LP%d;", copies, paperMode, leftMargin, cellsPerLine, topMargin, linesPerPage);
 		header = ByteSource.wrap(headerString.getBytes(Charsets.US_ASCII));
 	}
 	@Override
--- libembosser-core-drivers/src/test/java/org/brailleblaster/libembosser/drivers/indexBraille/IndexBrailleDocumentHandlerTest.java
+++ libembosser-core-drivers/src/test/java/org/brailleblaster/libembosser/drivers/indexBraille/IndexBrailleDocumentHandlerTest.java
@@ -24,6 +24,7 @@ import org.brailleblaster.libembosser.drivers.utils.DocumentHandler.StartLineEve
 import org.brailleblaster.libembosser.drivers.utils.DocumentHandler.StartPageEvent;
 import org.brailleblaster.libembosser.drivers.utils.DocumentHandler.StartSectionEvent;
 import org.brailleblaster.libembosser.drivers.utils.DocumentHandler.StartVolumeEvent;
+import org.brailleblaster.libembosser.spi.MultiSides;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
@@ -37,19 +38,20 @@ public class IndexBrailleDocumentHandlerTest {
 	@DataProvider(name="handlerProvider")
 	public Iterator<Object[]> handlerProvider() {
 		List<Object[]> data = new ArrayList<>();
-		final String basicHeader = "\u001bDBT0,MC1,BI%d,CH%d,TM%d,LP%d;";
+		final String basicHeader = "\u001bDBT0,MC1,DP1,BI%d,CH%d,TM%d,LP%d;";
 		final ImmutableList<DocumentEvent> minimalDocumentInput = ImmutableList.of(new StartDocumentEvent(), new StartVolumeEvent(), new StartSectionEvent(), new StartPageEvent(), new EndPageEvent(), new EndSectionEvent(), new EndVolumeEvent(), new EndDocumentEvent());
 		final String minimalDocumentOutput = basicHeader.concat("\f");
 		data.add(new Object[] {createHandlerBuilder().build(), minimalDocumentInput, String.format(minimalDocumentOutput, 0, 40, 0, 25)});
 		data.add(new Object[] {createHandlerBuilder().setLinesPerPage(30).build(), minimalDocumentInput, String.format(minimalDocumentOutput, 0, 40, 0, 30)});
 		final ImmutableList<DocumentEvent> basicDocumentInput = ImmutableList.of(new StartDocumentEvent(), new StartVolumeEvent(), new StartSectionEvent(), new StartPageEvent(), new StartLineEvent(), new BrailleEvent(",a te/ docu;t4"), new EndLineEvent(), new EndPageEvent(), new EndSectionEvent(), new EndVolumeEvent(), new EndDocumentEvent());
-		final String basicDocumentOutput = basicHeader.concat(",A TE/ DOCU;T4\f");
+		final String basicDocumentOutput = ",A TE/ DOCU;T4\f";
+		final String basicDocumentOutputWithHeader = basicHeader.concat(basicDocumentOutput);
 		final ImmutableList<DocumentEvent> basicCapsDocumentInput = ImmutableList.of(new StartDocumentEvent(), new StartVolumeEvent(), new StartSectionEvent(), new StartPageEvent(), new StartLineEvent(), new BrailleEvent(",A TE/ DOCU;T4"), new EndLineEvent(), new EndPageEvent(), new EndSectionEvent(), new EndVolumeEvent(), new EndDocumentEvent());
 		final ImmutableList<DocumentEvent> basicUnicodeDocumentInput = ImmutableList.of(new StartDocumentEvent(), new StartVolumeEvent(), new StartSectionEvent(), new StartPageEvent(), new StartLineEvent(), new BrailleEvent("\u2820\u2801\u2800\u281e\u2811\u280c\u2800\u2819\u2815\u2809\u2825\u2830\u281e\u2832"), new EndLineEvent(), new EndPageEvent(), new EndSectionEvent(), new EndVolumeEvent(), new EndDocumentEvent());
-		data.add(new Object[] {createHandlerBuilder().build(), basicDocumentInput, String.format(basicDocumentOutput, 0, 40, 0, 25)});
-		data.add(new Object[] {createHandlerBuilder().build(), basicCapsDocumentInput, String.format(basicDocumentOutput, 0, 40, 0, 25)});
-		data.add(new Object[] {createHandlerBuilder().build(), basicUnicodeDocumentInput, String.format(basicDocumentOutput, 0, 40, 0, 25)});
-		data.add(new Object[] {createHandlerBuilder().setLinesPerPage(28).build(), basicDocumentInput, String.format(basicDocumentOutput, 0, 40, 0, 28)});
+		data.add(new Object[] {createHandlerBuilder().build(), basicDocumentInput, String.format(basicDocumentOutputWithHeader, 0, 40, 0, 25)});
+		data.add(new Object[] {createHandlerBuilder().build(), basicCapsDocumentInput, String.format(basicDocumentOutputWithHeader, 0, 40, 0, 25)});
+		data.add(new Object[] {createHandlerBuilder().build(), basicUnicodeDocumentInput, String.format(basicDocumentOutputWithHeader, 0, 40, 0, 25)});
+		data.add(new Object[] {createHandlerBuilder().setLinesPerPage(28).build(), basicDocumentInput, String.format(basicDocumentOutputWithHeader, 0, 40, 0, 28)});
 		
 		final ImmutableList<DocumentEvent> multiLineDocumentInput = ImmutableList.of(new StartDocumentEvent(), new StartVolumeEvent(), new StartSectionEvent(), new StartPageEvent(), new StartLineEvent(), new BrailleEvent(",! F/ L9E4"), new EndLineEvent(), new StartLineEvent(), new BrailleEvent(",second l9e4"), new EndLineEvent(), new StartLineEvent(), new BrailleEvent(",a ?ird l9e4"), new EndLineEvent(), new EndPageEvent(), new EndSectionEvent(), new EndVolumeEvent(), new EndDocumentEvent());
 		final String[] multiLineDocumentOutputString = new String[] {",! F/ L9E4", ",SECOND L9E4", ",A ?IRD L9E4"};
@@ -68,13 +70,14 @@ public class IndexBrailleDocumentHandlerTest {
 		// Tests for adding/padding margins
 		data.add(new Object[] {createHandlerBuilder().setLeftMargin(3).setTopMargin(2).build(), multiPageDocumentInput, String.format(basicHeader, 3, 40, 2, 25) + Arrays.stream(multiPageDocumentOutputStrings).map(s -> String.format("\r\n\r\n   %s%s", s, "\f")).collect(StringBuilder::new, StringBuilder::append, StringBuilder::append).toString()});
 		// Multiple copy tests
-		final String copiesHeader = "\u001bDBT0,MC%d,BI%d,CH%d,TM%d,LP%d;%s";
+		final String copiesHeader = "\u001bDBT0,MC%d,DP1,BI%d,CH%d,TM%d,LP%d;%s";
 		data.add(new Object[] {createHandlerBuilder().setCopies(2).build(), multiPageDocumentInput, String.format(copiesHeader, 2, 0, 40, 0, 25, Arrays.stream(multiPageDocumentOutputStrings).map(s -> s.concat("\f")).collect(StringBuilder::new, StringBuilder::append, StringBuilder::append).toString())});
 		data.add(new Object[] {createHandlerBuilder().setCopies(2).setLinesPerPage(30).build(), multiPageDocumentInput, String.format(copiesHeader, 2, 0, 40, 0, 30, Arrays.stream(multiPageDocumentOutputStrings).map(s -> s.concat("\f")).collect(StringBuilder::new, StringBuilder::append, StringBuilder::append).toString())});
 		data.add(new Object[] {createHandlerBuilder().setCopies(4).build(), multiPageDocumentInput, String.format(copiesHeader, 4, 0, 40, 0, 25, Arrays.stream(multiPageDocumentOutputStrings).map(s -> s.concat("\f")).collect(StringBuilder::new, StringBuilder::append, StringBuilder::append).toString())});
 		data.add(new Object[] {createHandlerBuilder().setCopies(3).setLinesPerPage(30).build(), multiPageDocumentInput, String.format(copiesHeader, 3, 0, 40, 0, 30, Arrays.stream(multiPageDocumentOutputStrings).map(s -> s.concat("\f")).collect(StringBuilder::new, StringBuilder::append, StringBuilder::append).toString())});
 		// Tests for adding/padding margins
 		data.add(new Object[] {createHandlerBuilder().setCopies(11).setLeftMargin(3).setTopMargin(2).build(), multiPageDocumentInput, String.format(copiesHeader, 11, 3, 40, 2, 25, Arrays.stream(multiPageDocumentOutputStrings).map(s -> String.format("\r\n\r\n   %s%s", s, "\f")).collect(StringBuilder::new, StringBuilder::append, StringBuilder::append).toString())});
+		
 		return data.iterator();
 	}
 	@Test(dataProvider="handlerProvider")
@@ -87,6 +90,38 @@ public class IndexBrailleDocumentHandlerTest {
 			actual = handler.asByteSource().read();
 		} catch (IOException e) {
 			fail("Problem getting stream from handler");
+		}
+		assertEquals(actual, expected.getBytes(Charsets.US_ASCII));
+	}
+	@DataProvider(name="paperModeProvider")
+	public Iterator<Object[]> paperModeProvider() {
+		List<Object[]> data = new ArrayList<>();
+		final ImmutableList<DocumentEvent> basicDocumentInput = ImmutableList.of(new StartDocumentEvent(), new StartVolumeEvent(), new StartSectionEvent(), new StartPageEvent(), new StartLineEvent(), new BrailleEvent(",a te/ docu;t4"), new EndLineEvent(), new EndPageEvent(), new EndSectionEvent(), new EndVolumeEvent(), new EndDocumentEvent());
+		// Test the paper format
+		final String basicDocumentOutput = ",A TE/ DOCU;T4\f";
+		final String paperFormatHeader = "\u001bDBT0,MC1,DP%d,BI0,CH40,TM0,LP25;%s";
+		data.add(new Object[] {createHandlerBuilder(), MultiSides.P1ONLY, basicDocumentInput, String.format(paperFormatHeader, 1, basicDocumentOutput)});
+		data.add(new Object[] {createHandlerBuilder(), MultiSides.P2ONLY, basicDocumentInput, String.format(paperFormatHeader, 1, basicDocumentOutput)});
+		data.add(new Object[] {createHandlerBuilder(), MultiSides.INTERPOINT, basicDocumentInput, String.format(paperFormatHeader, 2, basicDocumentOutput)});
+		data.add(new Object[] {createHandlerBuilder(), MultiSides.Z_FOLDING_DOUBLE_HORIZONTAL, basicDocumentInput, String.format(paperFormatHeader, 3, basicDocumentOutput)});
+		data.add(new Object[] {createHandlerBuilder(), MultiSides.SADDLE_STITCH_DOUBLE_SIDED, basicDocumentInput, String.format(paperFormatHeader, 4, basicDocumentOutput)});
+		data.add(new Object[] {createHandlerBuilder(), MultiSides.Z_FOLDING_SINGLE_HORIZONTAL, basicDocumentInput, String.format(paperFormatHeader, 5, basicDocumentOutput)});
+		data.add(new Object[] {createHandlerBuilder(), MultiSides.Z_FOLDING_DOUBLE_VERTICAL, basicDocumentInput, String.format(paperFormatHeader, 6, basicDocumentOutput)});
+		data.add(new Object[] {createHandlerBuilder(), MultiSides.Z_FOLDING_SINGLE_VERTICAL, basicDocumentInput, String.format(paperFormatHeader, 7, basicDocumentOutput)});
+		data.add(new Object[] {createHandlerBuilder(), MultiSides.SADDLE_STITCH_SINGLE_SIDED, basicDocumentInput, String.format(paperFormatHeader, 8, basicDocumentOutput)});
+		return data.iterator();
+	}
+	@Test(dataProvider="paperModeProvider")
+	public void testPaperModeSetting(IndexBrailleDocumentHandler.Builder builder, MultiSides paperMode, List<DocumentEvent> eventsInput, String expected) {
+		IndexBrailleDocumentHandler handler = builder.setPaperMode(paperMode).build();
+		for (DocumentEvent event: eventsInput) {
+			handler.onEvent(event);
+		}
+		byte[] actual = null;
+		try {
+			actual = handler.asByteSource().read();
+		} catch (IOException e) {
+			fail("Problem reading output from handler");
 		}
 		assertEquals(actual, expected.getBytes(Charsets.US_ASCII));
 	}
