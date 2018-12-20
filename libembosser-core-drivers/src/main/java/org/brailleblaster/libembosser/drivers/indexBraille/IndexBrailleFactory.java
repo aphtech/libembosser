218e8f4fd7400c2fe4d532cf9710e27b249898e9
Michael Whapples <mwhapples@aim.com>
2018-12-20 08:58:56 +0000

    Add P2ONLY for index embossers.

--- libembosser-core-drivers/src/main/java/org/brailleblaster/libembosser/drivers/indexBraille/IndexBrailleFactory.java
+++ libembosser-core-drivers/src/main/java/org/brailleblaster/libembosser/drivers/indexBraille/IndexBrailleFactory.java
@@ -19,7 +19,7 @@ public class IndexBrailleFactory implements IEmbosserFactory {
 	private static final EnumSet<Layout> EVEREST_SIDES = EnumSet.of(Layout.INTERPOINT, Layout.P1ONLY, Layout.SADDLE_STITCH_DOUBLE_SIDED, Layout.SADDLE_STITCH_SINGLE_SIDED);
 	private static final EnumSet<Layout> BRAILLEBOX_SIDES = EVEREST_SIDES;
 	private static final EnumSet<Layout> FANFOLD_SIDES = BASIC_D_SIDES;
-	private static final EnumSet<Layout> ENABLING_SIDES = EnumSet.of(Layout.INTERPOINT, Layout.P1ONLY, Layout.Z_FOLDING_DOUBLE_VERTICAL, Layout.Z_FOLDING_SINGLE_VERTICAL, Layout.Z_FOLDING_DOUBLE_HORIZONTAL, Layout.Z_FOLDING_SINGLE_HORIZONTAL);
+	private static final EnumSet<Layout> ENABLING_SIDES = EnumSet.of(Layout.INTERPOINT, Layout.P1ONLY, Layout.P2ONLY, Layout.Z_FOLDING_DOUBLE_VERTICAL, Layout.Z_FOLDING_SINGLE_VERTICAL, Layout.Z_FOLDING_DOUBLE_HORIZONTAL, Layout.Z_FOLDING_SINGLE_HORIZONTAL);
 	private static final Rectangle FOUR_BY_THREE_PAPER = new Rectangle(new BigDecimal("101.6"), new BigDecimal("76.2"));
 	private static final Rectangle TWELVE_AND_HALF_BY_TWENTY_FOUR_PAPER = new Rectangle(new BigDecimal("317.5"), new BigDecimal("609.6"));
 	private static final Rectangle ELEVEN_AND_HALF_BY_ELEVEN_PAPER = PaperSize.BRAILLE_11_5X11.getSize();
