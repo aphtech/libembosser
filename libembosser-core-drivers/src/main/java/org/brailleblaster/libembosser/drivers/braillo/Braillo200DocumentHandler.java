464a4998b88e08c511a2bfd9d9a890090ec0c2bc
Michael Whapples <mwhapples@aim.com>
2019-04-09 08:22:30 +0100

    Braillo 200 set cells per line.

--- libembosser-core-drivers/src/main/java/org/brailleblaster/libembosser/drivers/braillo/Braillo200DocumentHandler.java
+++ libembosser-core-drivers/src/main/java/org/brailleblaster/libembosser/drivers/braillo/Braillo200DocumentHandler.java
@@ -4,16 +4,19 @@ import static com.google.common.base.Preconditions.checkArgument;
 import org.brailleblaster.libembosser.drivers.generic.GenericTextDocumentHandler;
 import org.brailleblaster.libembosser.drivers.utils.DocumentToByteSourceHandler;
 
+import com.google.common.base.Charsets;
 import com.google.common.io.ByteSource;
 
 public class Braillo200DocumentHandler implements DocumentToByteSourceHandler {
 	public static class Builder {
+		private int cellsPerLine = 40;
 		public Braillo200DocumentHandler build() {
-			return new Braillo200DocumentHandler();
+			return new Braillo200DocumentHandler(cellsPerLine);
 		}
 
 		public Builder setCellsperLine(int cellsPerLine) {
 			checkArgument(10 <= cellsPerLine && cellsPerLine <= 42);
+			this.cellsPerLine = cellsPerLine;
 			return this;
 		}
 		public Builder setSheetLength(double inches) {
@@ -21,15 +24,19 @@ public class Braillo200DocumentHandler implements DocumentToByteSourceHandler {
 			checkArgument(3.5 < inches && inches <= 14.0);
 			return this;
 		}
+		public Builder setInterpoint(boolean interpoint) {
+			return this;
+		}
 	}
 	private ByteSource headerSource;
 	private GenericTextDocumentHandler handler;
-	private Braillo200DocumentHandler() {
+	private Braillo200DocumentHandler(int cellsPerLine) {
 		handler = new GenericTextDocumentHandler.Builder()
+				.setCellsPerLine(cellsPerLine)
 				.padWithBlankLines(true)
 				.setEndOfPage(new byte[] {'\r', '\n', '\f'})
 				.build();
-		headerSource = ByteSource.wrap(new byte[] {0x1b, 'S', '1', 0x1b, 'J', '0', 0x1b, 'N', '0'});
+		headerSource = ByteSource.wrap(String.format("\u001bS1\u001bJ0\u001bN0\u001bB%02d", cellsPerLine).getBytes(Charsets.US_ASCII));
 	}
 	@Override
 	public void onEvent(DocumentEvent event) {
--- libembosser-core-drivers/src/test/java/org/brailleblaster/libembosser/drivers/braillo/Braillo200DocumentHandlerTest.java
+++ libembosser-core-drivers/src/test/java/org/brailleblaster/libembosser/drivers/braillo/Braillo200DocumentHandlerTest.java
@@ -5,9 +5,12 @@ import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException
 import static org.assertj.core.api.Assertions.fail;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Random;
+import java.util.stream.Collectors;
 import java.util.stream.DoubleStream;
 import java.util.stream.IntStream;
 
@@ -33,6 +36,7 @@ import com.google.common.collect.Range;
 import com.google.common.collect.Streams;
 
 public class Braillo200DocumentHandlerTest {
+	private static final Range<Integer> VALID_CELLS_PER_LINE = Range.closed(10, 42);
 	@Test
 	public void testDocumentBody() {
 		Braillo200DocumentHandler handler = new Braillo200DocumentHandler.Builder().build();
@@ -55,13 +59,47 @@ public class Braillo200DocumentHandlerTest {
 	@DataProvider(name="invalidCellsPerLineProvider")
 	public Iterator<Object[]> invalidCellsPerLineProvider() {
 		Random r = new Random(System.currentTimeMillis());
-		Range<Integer> validCellsPerLine = Range.closed(10, 42);
-		return Streams.concat(IntStream.of(7, 8, 9, 43, 44, 45), r.ints().filter(i -> !validCellsPerLine.contains(i)))
+		return Streams.concat(IntStream.of(7, 8, 9, 43, 44, 45), r.ints().filter(i -> !VALID_CELLS_PER_LINE.contains(i)))
 				.limit(100).mapToObj(i -> new Object[] { new Braillo200DocumentHandler.Builder(), i}).iterator();
 	}
 	@Test(dataProvider="invalidCellsPerLineProvider")
 	public void testSetCellsPerLineInvalidValueThrowsException(Braillo200DocumentHandler.Builder builder, int cellsPerLine) {
 		assertThatIllegalArgumentException().isThrownBy(() -> builder.setCellsperLine(cellsPerLine));
+	}
+	@DataProvider(name="validCellsPerLineProvider")
+	public Iterator<Object[]> validCellsPerLineProvider() {
+		List<Object[]> data = new ArrayList<>();
+		String[] inputLines = new String[] {
+				Strings.repeat("\u2800\u2801\u2802\u2803\u2804\u2805\u2806\u2807\u2808\u2809", 5),
+				"\u2811\u2812\u2813"
+		};
+		String[] outputLines = new String[] {
+				Strings.repeat(" A1B'K2L@C", 5),
+				"E3H"
+		};
+		for (int i = 10; i <= 42; i++) {
+			ImmutableList.Builder<DocumentEvent> eventsBuilder = ImmutableList.builder();
+			eventsBuilder.add(new StartDocumentEvent(), new StartVolumeEvent(), new StartSectionEvent(), new StartPageEvent());
+			for (String line : inputLines) {
+				eventsBuilder.add(new StartLineEvent(), new BrailleEvent(line), new EndLineEvent());
+			}
+			eventsBuilder.add(new EndPageEvent(), new EndSectionEvent(), new EndVolumeEvent(), new EndDocumentEvent());
+			final int lineLength = i;
+			String expectedBody = Arrays.stream(outputLines).map(s -> s.substring(0, Math.min(s.length(), lineLength)).concat("\r\n")).collect(Collectors.joining());
+			data.add(new Object[] { new Braillo200DocumentHandler.Builder(), i, eventsBuilder.build(), expectedBody});
+		}
+		return data.iterator();
+	}
+	@Test(dataProvider="validCellsPerLineProvider")
+	public void testSetCellsPerLine(Braillo200DocumentHandler.Builder builder, int cellsPerLine, List<DocumentEvent> events, String expectedBody) throws IOException {
+		Braillo200DocumentHandler handler = builder.setCellsperLine(cellsPerLine).build();
+		for (DocumentEvent event : events) {
+			handler.onEvent(event);
+		}
+		String actual = handler.asByteSource().asCharSource(Charsets.US_ASCII).read();
+		assertThat(actual)
+		.contains(String.format("\u001bB%02d", cellsPerLine))
+		.contains(expectedBody);
 	}
 	@DataProvider(name="invalidSheetLengthProvider")
 	public Iterator<Object[]> invalidSheetLengthProvider() {
